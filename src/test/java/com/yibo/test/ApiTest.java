28f76bd87f76d337635e91397633460dc4638eb8
XuYibo <xuyibo@outlook.com>
2018-11-15 13:37:56 +0800

    模拟压力测试

--- src/test/java/com/yibo/test/ApiTest.java
+++ src/test/java/com/yibo/test/ApiTest.java
@@ -9,8 +9,15 @@ import com.yibo.http.domain.response.Driver;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
 
 public class ApiTest {
+    private static final int CLIENT_TOTAL = 500;
+    private static final int THREAD_TOTAL = 50;
+
     @Test
     public void test01() throws IOException {
         Result<Driver> result = ApiClientUtil.get(HttpHost.VELP_HOST + "/driver/findByUserId/11", Driver.class);
@@ -18,7 +25,31 @@ public class ApiTest {
     }
 
     @Test
-    public void test02() throws IOException {
+    public void test02() {
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(THREAD_TOTAL);
+        final CountDownLatch countDownLatch = new CountDownLatch(CLIENT_TOTAL);
+        for (int i = 0; i < CLIENT_TOTAL; i++) {
+            threadPool.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    testPost();
+                    semaphore.release();
+                } catch (InterruptedException | IOException e) {
+                    e.printStackTrace();
+                }
+                countDownLatch.countDown();
+            });
+        }
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        threadPool.shutdown();
+    }
+
+    private void testPost() throws IOException {
         Result<Contracts> result = ApiClientUtil.post(
                 HttpHost.VELP_HOST + "/contract/query",
                 Contracts.class,
