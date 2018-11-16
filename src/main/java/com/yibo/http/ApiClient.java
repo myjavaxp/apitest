abf7555fa221deb8d1ed7617a956f130bc6e7fcc
XuYibo <xuyibo@outlook.com>
2018-11-16 08:48:20 +0800

    补充JavaDoc

--- src/main/java/com/yibo/http/ApiClient.java
+++ src/main/java/com/yibo/http/ApiClient.java
@@ -9,46 +9,34 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClients;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.util.CollectionUtils;
 
 import java.io.IOException;
 import java.util.Map;
 
 public class ApiClient {
     public static CloseableHttpResponse get(String url) throws IOException {
-        if (url == null || url.length() < 1) {
-            return null;
-        }
         return HttpClients.createDefault().execute(new HttpGet(url));
     }
 
     public static CloseableHttpResponse get(String url, Map<String, String> headers) throws IOException {
-        if (url == null || url.length() < 1) {
-            return null;
-        }
         HttpGet get = new HttpGet(url);
-        if (headers != null && headers.size() > 0) {
+        if (!CollectionUtils.isEmpty(headers)) {
             headers.forEach(get::addHeader);
         }
         return HttpClients.createDefault().execute(get);
     }
 
     public static <T> CloseableHttpResponse post(String url, T content) throws IOException {
-        if (url == null || url.length() < 1) {
-            return null;
-        }
         HttpPost post = new HttpPost(url);
         post.addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-        post.addHeader(HttpHeaders.CACHE_CONTROL,"no-cache");
         post.setEntity(new StringEntity(JSON.toJSONString(content), ContentType.APPLICATION_JSON));
         return HttpClients.createDefault().execute(post);
     }
 
     public static <T> CloseableHttpResponse post(String url, T content, Map<String, String> headers) throws IOException {
-        if (url == null || url.length() < 1) {
-            return null;
-        }
         HttpPost post = new HttpPost(url);
-        if (headers != null && headers.size() > 0) {
+        if (!CollectionUtils.isEmpty(headers)) {
             headers.forEach(post::addHeader);
         }
         post.setEntity(new StringEntity(JSON.toJSONString(content)));
--- src/main/java/com/yibo/http/ApiClientUtil.java
+++ src/main/java/com/yibo/http/ApiClientUtil.java
@@ -12,16 +12,43 @@ import org.springframework.http.HttpStatus;
 import java.io.IOException;
 
 public class ApiClientUtil {
+    /**
+     * get请求
+     *
+     * @param url        请求路径
+     * @param resultType 返回值类型，即{@link Result} 里的repData成员变量的类型
+     * @param <V>        同resultType
+     * @return 标准返回体Result内含类型为V的repData
+     * @throws IOException IO异常
+     */
     public static <V> Result<V> get(String url, Class<V> resultType) throws IOException {
         CloseableHttpResponse response = ApiClient.get(url);
         return handleResult(resultType, response);
     }
 
+    /**
+     * get请求 不知道repData类型或者没有给repData单独建类的时候使用
+     *
+     * @param url 请求路径
+     * @return 标准返回体Result内含类型为JSONObject的repData
+     * @throws IOException IO异常
+     */
     public static Result<JSONObject> get(String url) throws IOException {
         CloseableHttpResponse response = ApiClient.get(url);
         return handleResult(JSONObject.class, response);
     }
 
+    /**
+     * post请求
+     *
+     * @param url         请求路径
+     * @param resultType  返回值类型，即{@link Result} 里的repData成员变量的类型
+     * @param requestData 请求体内容，其实是{@link Request}内的成员变量reqData
+     * @param <V>         返回值类型，其实是{@link Result}里的repData的类型
+     * @param <T>         请求体类型，其实是{@link Request}内的成员变量reqData
+     * @return 标准返回体Result内含类型为V的repData
+     * @throws IOException IO异常
+     */
     public static <V, T> Result<V> post(String url, Class<V> resultType, T requestData) throws IOException {
         Request<T> request = new Request<>();
         request.setReqData(requestData);
@@ -29,11 +56,27 @@ public class ApiClientUtil {
         return handleResult(resultType, response);
     }
 
+    /**
+     * post请求 不知道repData,reqData类型或者没有给repData,reqData单独建类的时候使用
+     *
+     * @param url     请求路径
+     * @param request 包含类型JSONObject的Request的请求体
+     * @return 标准返回体Result内含类型为JSONObject的repData
+     * @throws IOException IO异常
+     */
     public static Result<JSONObject> post(String url, Request<JSONObject> request) throws IOException {
         CloseableHttpResponse response = ApiClient.post(url, request);
         return handleResult(JSONObject.class, response);
     }
 
+    /**
+     * post请求 不知道repData,reqData类型或者没有给repData,reqData单独建类的时候使用
+     *
+     * @param url     请求路径
+     * @param request 包含类型JSONObject的Request的请求体
+     * @return 标准返回体Result内含类型为JSONArray的repData
+     * @throws IOException IO异常
+     */
     public static Result<JSONArray> postForList(String url, Request<JSONObject> request) throws IOException {
         CloseableHttpResponse response = ApiClient.post(url, request);
         return handleResult(JSONArray.class, response);
